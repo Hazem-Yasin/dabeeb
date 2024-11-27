bcb814d32971fe8b678ada2113f6caa9da40dd9d
Hazem Yasin <hazem.yasin.1416@gmail.com>
2024-11-28 00:39:19 +0200

    opened the file

--- DabeebAPI/DabeebAPI/obj/Debug/net8.0/DabeebAPI.AssemblyInfo.cs
+++ DabeebAPI/DabeebAPI/obj/Debug/net8.0/DabeebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DabeebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b9adaef5840e5e305e20e9ee24ac848c460748")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a705735fb8a0a7fbe09cfbcd09275d0f44cad7f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DabeebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DabeebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
